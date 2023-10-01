2b7140492eca2015368fa852f9dd368042b0d2b3
mededaniela <mededaniela99@gmail.com>
2023-10-01 16:20:15 +0300

    SQLite task done

--- App.js
+++ App.js
@@ -1,37 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, TextInput, View, Text, Button, FlatList } from 'react-native';
-import { useState } from 'react';
-import { useWindowDimensions } from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  Button,
+  FlatList,
+} from "react-native";
+import { useState } from "react";
+import { useEffect } from "react";
+import * as SQLite from "expo-sqlite";
+import { TouchableOpacity } from "react-native";
 
 export default function App() {
-  const [data, setData] = useState([]);
-  const [text, setText] = useState("");
+  const [product, setProduct] = useState("");
+  const [amount, setAmount] = useState("");
+  const [shoppingList, setShoppingList] = useState([]);
 
-  const buttonPressedAdd = () => {
-    setData([...data, { key: text }]);
-    setText("");
+  const db = SQLite.openDatabase("ostoksetdb.db");
+
+  useEffect(() => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          "create table if not exists product (id integer primary key not null, product text, amount text);"
+        );
+      },
+      () => console.error("Error when creating DB"),
+      updateList
+    );
+  }, []);
+
+  const saveProduct = () => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql("insert into product (product, amount) values (?, ?);", [
+          product,
+          amount,
+        ]);
+      },
+      null,
+      updateList
+    );
   };
 
-  const buttonPressedClear = () => {
-    setData([]);
-  }
-
-  const { height, width, scale, fontScale } = useWindowDimensions();
+  const updateList = () => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql("select * from product;", [], (_, { rows }) =>
+          setShoppingList(rows._array)
+        );
+      },
+      null,
+      null
+    );
+  };
+  const deleteProduct = (id) => {
+    db.transaction((tx) => {
+      tx.executeSql("delete from product where id = ?;", [id]);
+      console.log("Error");
+      updateList();
+    });
+  };
 
   return (
     <View style={wholestyle.container}>
       <View style={styles.container}>
-        <TextInput style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={text => setText(text)} value={text} />
-      </View>
-      <View style={buttonstyles.container}>
-        <Button onPress={() => buttonPressedAdd()} title="Add" />
-        <Button onPress={() => buttonPressedClear()} title="Clear" />
+        <TextInput
+          placeholder="Product"
+          onChangeText={(text) => setProduct(text)}
+          value={product}
+        />
+        <TextInput
+          placeholder="Amount"
+          onChangeText={(text) => setAmount(text)}
+          value={amount}
+        />
+        <Button onPress={saveProduct} title="Save" />
       </View>
       <View>
         <Text>Shopping list:</Text>
-        <FlatList data={data} renderItem={({ item }) => <Text>{item.key}</Text>}
-          keyExtractor={(item, index) => index.toString()} />
+        <FlatList
+        data={shoppingList}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Text>
+              {item.product}, {item.amount}
+            </Text>
+            <TouchableOpacity onPress={() => deleteProduct(item.id)}>
+              <Text backgroundColor="red">bought</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
       </View>
     </View>
   );
@@ -46,16 +107,16 @@ const wholestyle = StyleSheet.create({
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
 const buttonstyles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    alignItems: 'space-between',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "space-between",
+    justifyContent: "center",
   },
 });
